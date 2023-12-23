de2ff18d2282527d722c34b24faf249b1439f24f
Jay Reddy <jayreddy@gmail.com>
2023-12-23 15:05:42 -0800

    unstages and cleans git history

--- online_retail_project/online_retail_app/apps.py
+++ online_retail_project/online_retail_app/apps.py
@@ -1,9 +1,5 @@
 from django.apps import AppConfig
-from django.core.management import call_command
 
 
 class YourAppConfig(AppConfig):
     name = 'online_retail_app'
-
-    def ready(self):
-        call_command('seeddb', '--csv_file', 'path/to/default/csvfile.csv')
--- online_retail_project/online_retail_app/management/commands/seed_db_if_no_db.py
+++ online_retail_project/online_retail_app/management/commands/seed_db_if_no_db.py
@@ -11,73 +11,71 @@ class Command(BaseCommand):
     help = 'Seeds the database from a CSV file'
 
     def handle(self, *args, **kwargs):
-        if InvoiceItem.objects.exists():
-            self.stdout.write(self.style.SUCCESS(
-                'Database is already seeded. No action taken.'))
-            return
-
-        # Constructing the path to the CSV file
         csv_file = os.path.join(
-            settings.BASE_DIR, 'data', 'online_retail_data.csv')
+            settings.BASE_DIR, 'data', 'online-retail-10k.csv')
 
         try:
             with open(csv_file, mode='r', encoding='utf-8-sig') as file:
                 reader = csv.DictReader(file)
+
+                for row in reader:
+                    try:
+                        customer_id_str = row['CustomerID']
+                        if customer_id_str:
+                            customer_id = int(float(customer_id_str))
+                            customer, created = Customer.objects.get_or_create(
+                                customer_id=customer_id)
+                            if not created:
+                                continue  # Skip if customer already exists
+                        else:
+                            customer = None
+
+                        product, created = Product.objects.get_or_create(
+                            stock_code=row['StockCode'],
+                            defaults={'description': row['Description']}
+                        )
+                        if not created:
+                            continue  # Skip if product already exists
+
+                        invoice_date_str = row['InvoiceDate']
+                        invoice_date = datetime.strptime(
+                            invoice_date_str, '%m/%d/%Y %H:%M')
+
+                        invoice, created = Invoice.objects.get_or_create(
+                            invoice_no=row['InvoiceNo'],
+                            defaults={
+                                'customer': customer,
+                                'invoice_date': invoice_date,
+                                'country': row['Country']
+                            }
+                        )
+                        if not created:
+                            continue  # Skip if invoice already exists
+
+                        InvoiceItem.objects.get_or_create(
+                            invoice=invoice,
+                            product=product,
+                            quantity=int(row['Quantity']),
+                            unit_price=float(row['UnitPrice'])
+                        )
+
+                        self.stdout.write(self.style.SUCCESS(
+                            f"Processed invoice {invoice.invoice_no}"))
+                    except IntegrityError as e:
+                        self.stdout.write(self.style.ERROR(
+                            f"Integrity error for row {row['index']}: {e}"))
+                    except ValueError as e:
+                        self.stdout.write(self.style.ERROR(
+                            f"Value error for row {row['index']}: {e}"))
+                    except Exception as e:
+                        self.stdout.write(self.style.ERROR(
+                            f"Unexpected error for row {row['index']}: {e}"))
+
         except FileNotFoundError:
             raise CommandError('CSV file not found at {}'.format(csv_file))
         except Exception as e:
             raise CommandError(
                 f'An error occurred while reading the file: {e}')
-
-        for row in reader:
-            try:
-                customer_id = int(
-                    row['CustomerID']) if row['CustomerID'] else None
-                if customer_id:
-                    customer, _ = Customer.objects.get_or_create(
-                        customer_id=customer_id)
-                else:
-                    customer = None
-
-                product, _ = Product.objects.get_or_create(
-                    stock_code=row['StockCode'],
-                    defaults={'description': row['Description']}
-                )
-
-                invoice_date_str = row['InvoiceDate']
-                try:
-                    invoice_date = datetime.strptime(
-                        invoice_date_str, '%m/%d/%Y %H:%M')
-                except ValueError:
-                    raise ValueError(
-                        f"Invalid date format for invoice {row['InvoiceNo']}")
-
-                invoice, _ = Invoice.objects.get_or_create(
-                    invoice_no=row['InvoiceNo'],
-                    defaults={
-                        'customer': customer,
-                        'invoice_date': invoice_date,
-                        'country': row['Country']
-                    }
-                )
-
-                InvoiceItem.objects.create(
-                    invoice=invoice,
-                    product=product,
-                    quantity=int(row['Quantity']),
-                    unit_price=float(row['UnitPrice'])
-                )
-                self.stdout.write(self.style.SUCCESS(
-                    f"Processed invoice {invoice.invoice_no}"))
-            except IntegrityError as e:
-                self.stdout.write(self.style.ERROR(
-                    f"Integrity error for row {row['index']}: {e}"))
-            except ValueError as e:
-                self.stdout.write(self.style.ERROR(
-                    f"Value error for row {row['index']}: {e}"))
-            except Exception as e:
-                self.stdout.write(self.style.ERROR(
-                    f"Unexpected error for row {row['index']}: {e}"))
 
         self.stdout.write(self.style.SUCCESS(
             'Data import completed successfully'))
